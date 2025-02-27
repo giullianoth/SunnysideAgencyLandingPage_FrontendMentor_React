d40f103abb6613195c86987dab9a143646c877bf
Giulliano <tharsoweb@gmail.com>
2025-02-27 17:23:13 -0300

    first configuration

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Footer from "./components/page/Footer"
+import Gallery from "./components/page/Gallery"
+import Header from "./components/page/Header"
+import Headline from "./components/page/Headline"
+import Presentation from "./components/page/Presentation"
+import Services from "./components/page/Services"
+import Testimonials from "./components/page/Testimonials"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Headline />
+        <Presentation />
+        <Services />
+        <Testimonials />
+        <Gallery />
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
